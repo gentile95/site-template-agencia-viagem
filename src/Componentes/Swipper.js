af9dc9ca3c0aa227803f67c1b637449d6b77946a
Lucas Gentile <lucas.gentile95@gmail.com>
2024-03-13 21:31:55 -0300

    updating serviços

--- src/Componentes/Swipper.js
+++ src/Componentes/Swipper.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper/modules';
-import './Swipper.css'
-
-// Import Swiper styles
-import 'swiper/css';
-
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import './Swipper.css';
 
 const Swipper = () => {
-
+  
   const fotos = [
     {id: '1', image: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
     {id: '2', image: 'https://images.pexels.com/photos/1813922/pexels-photo-1813922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
@@ -18,20 +14,28 @@ const Swipper = () => {
 
   return (
     <div className="container">
-      <Swiper
-        modules={[Navigation, Pagination]}
-        slidesPerView={2}
-        pagination={{clickable:true}}
-        navigation
-        scrollbar={{draggable:true}}
-      >
-        {fotos.map((foto) => (
-          <SwiperSlide key={foto.id}>
-            <img src={foto.image} alt="Foto de um dos nossos colaboradores" className='slide-item' />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+        <AliceCarousel
+          itemsToShow={2}
+          itemsToSlide={2}
+          responsive={true}
+          autoPlay={true}
+          infinite={true}
+          animationDuration={1500}
+          autoPlayInterval={1500}
+          items={2}
+          lazyLoad={true}
+        >
+            {fotos.map((foto) => (
+              <div className="container-img-2">
+                <div className='fotos' key={foto.id}>
+                  <img src={foto.image} alt="Foto de um dos nossos colaboradores" className='sliderimg'/>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores temporibus magni, ad hic non incidunt tempora porro repudiandae tempore, dolores iusto. Non laudantium aliquam sint et sequi adipisci iusto praesentium</p>
+              </div>
+            ))}
+          
+        </AliceCarousel>
+      </div>
   )
 }
 
--- src/routes/Info.js
+++ src/routes/Info.js
@@ -4,7 +4,7 @@ import HeroImg from '../Componentes/HeroImg'
 import assetInfo from '../assets/asset-sobre.jpg'
 import InfoDescricao from '../Componentes/InfoDescricao'
 import Footer from '../Componentes/Footer'
-import Swiper from '../Componentes/Swipper'
+import Swipper from '../Componentes/Swipper'
 
 const Info = () => {
   return (
@@ -17,7 +17,7 @@ const Info = () => {
         imgAlt = 'Duas pessoas no gelo, em um ambiente congelado, olhando para uma aurora, com montanhas congeladas ao fundo'
       />    
       <InfoDescricao />  
-      <Swiper />
+      <Swipper />
       <Footer />
     </>
   )
